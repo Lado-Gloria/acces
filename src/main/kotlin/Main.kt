5f8bca6f3b1bacf280ba34202f4d2aab8be8d660
Gloria-Lado <glorialadoo@gmail.com>
2023-03-09 08:58:34 +0300

    files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,35 @@
+fun main() {
+    var result = names("gloria","alice")
+    println(result.length)
+    number(10, 4,6)
+    name("akirachix")
+    var unity = start("glo","peter","hellen","unice"))
+    println(unity)
+    var cal =  Calculator(2,3,5)
+
+
+
+}
+fun names(nam1:String,nam2:String):String{
+    var name = (nam1)
+    return name
+}
+fun number(num1:Int, num2:Int,num3:Int):Int{
+  var numbers =(num1+num2)
+   return numbers
+
+
+
+}
+fun name(Name:String){
+    var name =("akirachix")
+    println(name[0]+ "" +  name[2] +"" +  name[3])
+}
+fun start(nam1:String,nam2:String,nam3:String,nam4:String):String{
+    var started= arrayOf(nam1,nam2,nam3,nam4)
+    return started.contentToString()}
+class Calculator(var num1:Int, var num2:Int,var num3:Int){
+    fun add(){
+
+    }
+}
